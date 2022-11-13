1628f5ff0a30d22e1fc4d2d60b2f00b69ce60f6b
apine <1985486089@qq.com>
2022-11-13 15:54:59 +0800

    :wrench: 新增规则，更新注释

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,13 +7,13 @@ module.exports = {
     extends: ['eslint-config-airbnb-base', 'plugin:prettier/recommended'],
     // 自定义规则
     rules: {
+        // 允许在package.json中不列出devDependencies包名，适用于monorepo
         'import/no-extraneous-dependencies': [
             'error',
             {
                 devDependencies: true
             }
         ],
-        'vue/multi-word-component-names': 0,
         // 取消对for...of的限制
         'no-restricted-syntax': [
             'error',
@@ -32,7 +32,11 @@ module.exports = {
                 message:
                     '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.'
             }
-        ]
+        ],
+        // 引入时可以使用后缀名
+        "import/extensions": ["error", "always", {
+            ignorePackages: false
+        }]
     },
     // File ignored by default.  Use a negated ignore pattern
     ignorePatterns: ['!.*', 'dist', 'node_modules']
